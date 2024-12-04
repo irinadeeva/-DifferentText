fd186819f58c9b1f54a2d036805d4b266692d597
irinadeeva <deeva97@me.com>
2024-12-04 09:32:12 +0300

    Initial Commit

---
+++ DifferentText/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DifferentText
+//
+//  Created by Irina Deeva on 04/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DifferentText/DifferentTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  DifferentTextApp.swift
+//  DifferentText
+//
+//  Created by Irina Deeva on 04/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DifferentTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
