ea5aed409d9a398ff122c59108ee17c715298ed8
irinadeeva <deeva97@me.com>
2024-12-04 16:50:59 +0500

    done

--- DifferentText/ContentView.swift
+++ DifferentText/ContentView.swift
@@ -8,17 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+  @State var width: CGFloat = 300
+  var body: some View {
+    VStack {
+
+      VStack {
+
+        Text("\(Text("Марафон").foregroundStyle(.gray)) по SwiftUI \(Text("\"Отцовский Пинок\"").bold().foregroundStyle(.blue).font(.title))")
+
+      }
+      .frame(height: 200)
+      .frame(width: width)
+      .border(.red)
+
+      Slider(value: $width, in: 100...300)
         .padding()
     }
+    .padding()
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
